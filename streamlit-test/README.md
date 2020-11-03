73b5478f64d06105490bb6ee0693cc6b52abaf2d
Eliaser <eliaserdev99@gmail.com>
2020-11-02 23:08:41 -0300

    Streamlit Test: Pacientes UCI por rango etario (test) added
    Esto es una prueba de gráficos y tablas utilizando el archivo csv del producto 9.

--- streamlit-test/README.md
+++ streamlit-test/README.md
@@ -1,7 +1,14 @@
 # Proyecto de Prueba con Streamlit
 
-> Este es un proyecto de prueba que utiliza como tecnología principal Python y su librería de streamlit, además se utiliza las diferentes API's ofrecidas por la NASA para mostrar algo de información con esta librería
+> Este es un proyecto de prueba que utiliza como tecnología principal Python y su librería de streamlit, además se utiliza las diferentes API's ofrecidas por la NASA para mostrar algo de información con esta librería. Por otro lado, se realizan pruebas con algunas datos sobre la enfermedad Sars-Cov2 en Chile.
 
 ## API's utilizadas
 
 * APOD (Astronomy Picture of the Day): Muestra una imagen al día.
+
+## CSV's utilizados
+
+## Comandos a utilizar
+
+* pipenv shell -> Arranca el entorno virtual
+* streamlit run index.py -> Inicia toda la aplicación abriendo una pestaña en el navegador
--- streamlit-test/index.py
+++ streamlit-test/index.py
@@ -1,14 +1,41 @@
 import streamlit as st
-import funciones as fun
+import api as api
+import datos_covid as covid
+
+def index():
+    st.title('Sobre este proyecto')
 
 if __name__=='__main__':
-    menu = st.sidebar.selectbox(
-        "API's",
-        ['Elija una opción','APOD','InSight']
+    options = st.sidebar.selectbox(
+        'Obtener Datos',
+        ['Elija una opción','API Nasa','Datos Covid']
     )
-    if menu=='Elija una opción':
-        fun.index()
-    elif menu=='APOD':
-        fun.obtener_imagen()
-    else:
-        st.write('API no encontrada')
+    if options=='Elija una opción':
+        index()
+    elif options=='API Nasa':
+        menu = st.sidebar.selectbox(
+            "API's",
+            ['Elija una opción','APOD','InSight']
+        )
+        if menu=='Elija una opción':
+            api.index()
+        elif menu=='APOD':
+            api.obtener_imagen()
+        else:
+            st.write('API no encontrada')
+    elif options=='Datos Covid':
+        busqueda = st.sidebar.selectbox(
+            'Parámetro a mostrar',
+            ['Elija una opción','Pacientes UCI por rango etario']
+        )
+        if busqueda=='Elija una opción':
+            covid.index()
+        elif busqueda=='Pacientes UCI por rango etario':
+            options = st.sidebar.selectbox(
+                'Opción a mostrar',
+                ['Tabla','Gráfico']
+            )
+            if options=='Tabla':
+                covid.tabla_pacientes_uci()
+            elif options=='Gráfico':
+                covid.grafico_pacientes_uci()
